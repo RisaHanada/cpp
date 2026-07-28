69cda5323f48936423de5b163f6276a3ca1139de
RISA HANADA <risahanada18@gmail.com>
2026-07-28 17:25:29 -0400

    add constant values

---
+++ oop-restaurant-ordering-system/constants.h
@@ -0,0 +1,15 @@
+/***********************************************************************
+// File	constrants.h
+// Date	2025/11/2
+// Author Fardad
+***********************************************************************/
+#ifndef SENECA_CONSTANTS_H
+#define SENECA_CONSTANTS_H
+
+namespace seneca {
+	const size_t MaximumNumberOfMenuItems = 20u;
+	const double Tax = 0.13;
+	const size_t MaximumNumberOfBillItems = 20u;
+}
+
+#endif
