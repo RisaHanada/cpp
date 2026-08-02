65a7230c9ce550f4991cb1c9d4425fcb69884203
RISA HANADA <risahanada18@gmail.com>
2026-08-02 15:21:16 -0400

    add setting.cpp implementation

---
+++ oop-dictionary/settings.cpp
@@ -0,0 +1,15 @@
+/***********************************************************************
+// settings Module
+// File	settings.cpp
+// Date	2026/01/29 
+// Author Risa Hanada
+***********************************************************************/
+#include "settings.h"
+
+namespace seneca {
+    /*
+    Defines the global Settings instance (g_settings) declared in settings.h.
+    This object stores the configuration options used throughout the dictionary application.
+    */
+	Settings g_settings{};
+}
