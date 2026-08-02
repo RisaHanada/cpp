aea44d6ea64392eb8f083e993587febc00635572
RISA HANADA <risahanada18@gmail.com>
2026-08-02 15:18:18 -0400

    add comment

--- oop-dictionary/settings.h
+++ oop-dictionary/settings.h
@@ -10,11 +10,11 @@
 
 namespace seneca {
 	struct Settings {
-		bool m_show_all = false;
-		bool m_verbose = false;
-		std::string m_time_units = "nanoseconds";
+		bool m_show_all = false; // when true, if a word has multiple definitions, all definitions should be printed on screen, otherwise only the first definition should be shown (default false).
+		bool m_verbose = false; // when true, print to screen the part-of-speech of a word if it exists (default false).
+		std::string m_time_units = "nanoseconds"; // stores the time units to be used when printing duration of various operations (default nanoseconds). 
 	};
-	extern Settings g_settings;
+	extern Settings g_settings; // all configuration options are stored in a global instance (g_settings)
 }
 
 #endif
